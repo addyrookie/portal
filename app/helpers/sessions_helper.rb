52d1b9e5777265f41e8152b138a1b01bd6005973
addyrookie <adityadixit06@gmail.com>
2011-03-31 01:44:56 +0530

    root path tweaks

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,8 @@
 module SessionsHelper
+  
+   def current_user
+    @current_user ||= user_from_remember_token
+  end
 
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
@@ -9,15 +13,16 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
-
-  def current_user
-    @current_user ||= user_from_remember_token
-  end
+ 
   
   def signed_in?
     !current_user.nil?
   end
-  
+    
+   def current_user=(user)
+    @current_user = user
+  end
+
   
   def current_user?(user)
     user == current_user
