1a4ac442f403b4d98ee6e03ac16c02a45351b4df
addyrookie <adityadixit06@gmail.com>
2011-03-30 23:12:18 +0530

    user creation done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+  
+  def show 
   end
 
 end
